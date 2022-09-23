6c2eb4a84fe69849651dd6af5ed537df7346904d
Francisco Garcia Fio <fran_810@hotmail.com>
2022-09-22 23:17:05 -0300

    primer entrega garcia fio

---
+++ src/components/contenedor/itemListContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const cartWidget = () => {
+    return (
+        <i className="bi bi-cart-check"></i>
+    );
+  };
+  
+  export default cartWidget;
+
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import cartWidget from "../cartwidjet/cartWidjet";
+import {
+  Container,
+  LogoContainer,
+  Wrapper,
+  Menu,
+  MenuItem,
+  MenuItemLink,
+  MobileIcon,
+} from "./navbar.elements";
+import {
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
+
+import { IconContext } from "react-icons";
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  return (
+    <Container>
+      <Wrapper>
+        <IconContext.Provider value={{ style: { fontSize: "2em" } }}>
+          <LogoContainer>         
+            <p> F.G. SERVICIOS INMOBILIARIOS</p>                      
+          </LogoContainer>
+          <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            {showMobileMenu ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+          <Menu open={showMobileMenu}>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+               INICIO
+             </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                  VENTAS
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>                  
+                  ALQUILERES
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                  CONTACTANOS
+              </MenuItemLink>
+              </MenuItem>
+              <MenuItem>
+               <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <div>
+                <cartWidget />
+                </div>
+      
+            </MenuItemLink>
+            </MenuItem>
+          </Menu>
+        </IconContext.Provider>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
